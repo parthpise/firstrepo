8a822a6e0d2c1f9f8f2973ed6ea0132605aa7288
parth <parthpise521@gmail.com>
2024-03-09 23:56:02 +0530

    Todays program added 09/03/2023

--- src/test/java/Coding/Practice.java
+++ src/test/java/Coding/Practice.java
@@ -1,31 +1,22 @@
 package Coding;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Practice {
     public static void main(String [] args){
-        String a = "Parth";
-        String b = "Saish";
+       String a = "Parthaaaasssdddddppppppertyu";
 
-        a.toLowerCase();
-        b.toLowerCase();
+      int b = a.length();
+      int c = b-4;
 
-        if(a.length() != b.length()){
-            System.out.println("Given words are not anagrams");
-        }
-        else{
-            char[] first = a.toCharArray();
-            char[] second = b.toCharArray();
+      for(int i = c; i < b; i++){
+          System.out.print(a.charAt(i));
+      }
 
-            Arrays.sort(first);
-            Arrays.sort(second);
 
-            if(Arrays.equals(first,second) == true){
-                System.out.println("Given words are anagrams");
-            }
-            else{
-                System.out.println("They are not anagrams");
-            }
-        }
+
+
+
     }
 }
